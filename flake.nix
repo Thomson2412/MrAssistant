a9fa0b308baa8c9aba4726385eba80c753e54697
Thomson2412 <thomasm.w.fink@gmail.com>
2025-07-10 15:02:05 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "ESPHome Environment";
+  description = "ESPHome pip-based dev env";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,12 +10,31 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          pip
+          setuptools
+          wheel
+        ]);
       in {
         devShell = pkgs.mkShell {
-          name = "esphome-env";
-          buildInputs = [
-            pkgs.esphome
-          ];
+          name = "esphome-dev";
+
+          buildInputs = [ pythonEnv ];
+
+          shellHook = ''
+            if [ ! -d .venv ]; then
+              echo "Creating virtualenv in .venv..."
+              python3 -m venv .venv
+              source .venv/bin/activate
+              pip install --upgrade pip
+              pip install esphome
+            else
+              source .venv/bin/activate
+            fi
+
+            echo "ESPHome ready. Run with: esphome run your-config.yaml"
+          '';
         };
       });
 }
