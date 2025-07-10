fbaf96df8ab028e2ed87799545bf84d56b804ece
Thomson2412 <thomasm.w.fink@gmail.com>
2025-07-10 13:14:32 +0200

    Setup env

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "ESPHome Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          name = "esphome-env";
+          buildInputs = [
+            pkgs.esphome
+          ];
+        };
+      });
+}
